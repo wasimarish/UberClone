67f6c60b9529118dff366e819c8663b4e72aab20
wasimarish <arishwasim123@gmail.com>
2025-03-22 23:22:38 +0530

    middle of home page

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -1,42 +1,69 @@
 import React from 'react'
 
-export const LocationSearchPanel = () => {
+export const LocationSearchPanel = (props) => {
+
+  console.log(props)
+  const location=[
+    "123345 Maple Street, Imaginary City, State 98765, Planet Earth.",
+    "123547 Maple Street, Imaginary City, State 98765, Planet Earth.",
+    "12333 Maple Street, Imaginary City, State 98765, Planet Earth.",
+    "1244 Maple Street, Imaginary City, State 98765, Planet Earth.",
+  ]
   return (
-    <div className='p-4'>
-      <div className='flex flex-center justify-start my-3 gap-1.5'>
-      <h2 className='bg-[#eee] rounded-full h-6'><i className="ri-map-pin-line"></i></h2>
-        <h4 className='font-medium'> 
-          123 Maple Street, Imaginary City, State 98765, Planet Earth.
-        </h4>
-      </div>
 
-      <div className='flex flex-center justify-start my-3 gap-1.5'>
-      <h2 className='bg-[#eee] rounded-full h-6'><i className="ri-map-pin-line"></i></h2>
-        <h4 className='font-medium'> 
-          123 Maple Street, Imaginary City, State 98765, Planet Earth.
-        </h4>
-      </div>
+    location.map(function (address) {
+      return (
+        <div className="p-4" key={address}>
+          <div onClick={()=>{props.setVehiclePanel(true)
+                            props.setPanelopen(false)
+          }} className="active:border-2 flex items-center justify-start my-3 gap-1.5 rounded-xl">
+            <h2 className="bg-[#eee] rounded-full w-12 h-8 flex items-center justify-center mt-2">
+              <i className="ri-map-pin-line"></i>
+            </h2>
+            <h4 className="font-medium">{address}</h4>
+          </div>
+        </div>
+      );
+    })
+    
 
-      <div className='flex flex-center justify-start my-3 gap-1.5'>
-      <h2 className='bg-[#eee] rounded-full h-6'><i className="ri-map-pin-line"></i></h2>
-        <h4 className='font-medium'> 
-          123 Maple Street, Imaginary City, State 98765, Planet Earth.
-        </h4>
-      </div>
 
-      <div className='flex flex-center justify-start my-3 gap-1.5'>
-      <h2 className='bg-[#eee] rounded-full h-6'><i className="ri-map-pin-line"></i></h2>
-        <h4 className='font-medium'> 
-          123 Maple Street, Imaginary City, State 98765, Planet Earth.
-        </h4>
-      </div>
+    // {
+    //   location.map(function(){
+    //     return <div className='p-4'>
+    //     <div className=' active:border-2 flex flex-center justify-start my-3 gap-1.5 rounded-xl '>
+    //     <h2 className='bg-[#eee] rounded-full w-12 h-8 flex items-center justify-center mt-2'><i className="ri-map-pin-line"></i></h2>
+    //       <h4 className='font-medium'> 
+    //         123 Maple Street, Imaginary City, State 98765, Planet Earth.
+    //       </h4>
+    //     </div>
+    //   })
+    // }
 
-      <div className='flex flex-center justify-start my-3 gap-1.5'>
-      <h2 className='bg-[#eee] rounded-full h-6'><i className="ri-map-pin-line"></i></h2>
-        <h4> 
-        456 Elmwood Avenue, Unit 789, Sunnybrook Residences, Building C, Westside District, Metroville, State of Imagination, ZIP 24680, Country of Fictionland, Earth, Milky Way Galaxy.
-        </h4>
-      </div>
-    </div>
+
+    // <div className='p-4'>
+    //   <div className=' active:border-2 flex flex-center justify-start my-3 gap-1.5 rounded-xl '>
+    //   <h2 className='bg-[#eee] rounded-full w-12 h-8 flex items-center justify-center mt-2'><i className="ri-map-pin-line"></i></h2>
+    //     <h4 className='font-medium'> 
+    //       123 Maple Street, Imaginary City, State 98765, Planet Earth.
+    //     </h4>
+    //   </div>
+
+    //   <div className=' active:border-2 flex flex-center justify-start my-3 gap-1.5 rounded-xl '>
+    //   <h2 className='bg-[#eee] rounded-full w-12 h-8 flex items-center justify-center mt-2'><i className="ri-map-pin-line"></i></h2>
+    //     <h4 className='font-medium'> 
+    //       123 Maple Street, Imaginary City, State 98765, Planet Earth.
+    //     </h4>
+    //   </div>
+
+    //   <div className=' active:border-2 flex flex-center justify-start my-3 gap-1.5 rounded-xl '>
+    //   <h2 className='bg-[#eee] rounded-full w-12 h-8 flex items-center justify-center mt-2'><i className="ri-map-pin-line"></i></h2>
+    //     <h4 className='font-medium'> 
+    //       123 Maple Street, Imaginary City, State 98765, Planet Earth.
+    //     </h4>
+    //   </div>
+
+      
+    // </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,6 +10,8 @@ const Home = () => {
   const [panelopen,setPanelopen]=useState(false);
   const panelRef=useRef(null);
   const panelCloseRef=useRef(null);
+  const [vehiclePanelOpen,setVehiclePanelOpen]=useState(false);
+  const vehiclePanelRef = useRef(null)
 
   const submitHandler=(e)=>{
     e.preventDefault();
@@ -34,8 +36,22 @@ const Home = () => {
     }
   },[panelopen]);
 
+  useGSAP(function(){
+
+    if(vehiclePanelOpen){
+      gsap.to(vehiclePanelRef.current,{
+        transform:'translateY(0)'
+      })
+    }else{
+      gsap.to(vehiclePanelRef.current,{
+        transform:'translateY(100%)'
+      })
+    }
+   
+  },[vehiclePanelOpen])
+
   return (
-    <div className='h-screen relative ' >
+    <div className='h-screen relative overflow-hidden' >
       <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="Uber logo" />
 
       <div className='h-screen w-screen '>
@@ -67,12 +83,41 @@ const Home = () => {
 
         <div ref={panelRef} className='h-[70%] bg-white'>
           {/* Content for the panel can go here */}
-          <LocationSearchPanel/>
+          <LocationSearchPanel panelopen={panelopen} setPanelopen={setPanelopen} vehiclePanel={vehiclePanelOpen} setVehiclePanel={setVehiclePanelOpen}/>
         </div>
       </div>
-
-      <div className='z-10'>
-
+      {/* car */}
+      <div ref={vehiclePanelRef} className='w-full fixed z-10 bottom-0 bg-white px-3 py-6 translate-y-full '>
+         <div className='mb-2  w-full border-2 border-black rounded-xl flex items-center justify-evenly'>
+         <img className='h-14 'src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_538,w_956/v1688398971/assets/29/fbb8b0-75b1-4e2a-8533-3a364e7042fa/original/UberSelect-White.png" alt="" />
+          <div className=' w-1/2'>
+            <h4 className='font-bold text-m'>UberGo <span><i className="ri-user-3-line"></i> 4 </span></h4>
+            <h5 className='font-bold text-m'>2 mins away</h5>
+            <p className='font-medium text-gray-600 text-xs' >Affordable Comapct Rides</p>
+          </div>
+          <h2 className='text-2xl font-bold'>$22.4</h2>
+         </div>
+        {/* bike */}
+         <div className='mb-2 w-full border-2 border-black rounded-xl flex items-center justify-evenly'>
+         <img className='h-14 'src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_956,h_537/v1571927853/assets/39/c1c2c7-61eb-4432-9bac-728b974207e7/original/cityscoot-icon-mobile.png" alt="" />
+          <div className=' w-1/2'>
+            <h4 className='font-bold text-m'>MotorCycle <span><i className="ri-user-3-line"></i> 1</span></h4>
+            <h5 className='font-bold text-m'>2 mins away</h5>
+            <p className='font-medium text-gray-600 text-xs' >Affordable Comapct Rides</p>
+          </div>
+          <h2 className='text-2xl font-bold'>$233.4</h2>
+         </div>
+          {/* auto */}
+         <div className='mb-2 w-full border-2 border-black rounded-xl flex items-center justify-evenly'>
+         <img className='h-14 'src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="" />
+          <div className=' w-1/2'>
+            <h4 className='font-bold text-m'>AutoRickshaw<span><i className="ri-user-3-line"></i> 3 </span></h4>
+            <h5 className='font-bold text-m'>2 mins away</h5>
+            <p className='font-medium text-gray-600 text-xs' >Affordable Comapct Rides</p>
+          </div>
+          <h2 className='text-2xl font-bold'>$2.4</h2>
+         </div>
+         
       </div>
     </div>
   )
